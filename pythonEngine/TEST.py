122d05709493c7541981babfcadbdcc072a254ba
perii <jonathan.peri56@gmail.com>
2022-06-17 11:43:05 +0300

    changed back
    aaaaa

--- pythonEngine/TEST.py
+++ pythonEngine/TEST.py
@@ -1,113 +1,243 @@
-from turtle import position
-#from typing_extensions import Self
 import pygame as pg
 from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram,compileShader
 import numpy as np
-import ctypes
-from OpenGL.GL.shaders import compileProgram, compileShader
 import pyrr
-#app
 
 class Cube:
 
+
     def __init__(self, position, eulers):
+
         self.position = np.array(position, dtype=np.float32)
         self.eulers = np.array(eulers, dtype=np.float32)
+
+class Player:
+
+
+    def __init__(self, position):
+
+        self.position = np.array(position, dtype = np.float32)
+        self.theta = 0
+        self.phi = 0
+        self.update_vectors()
+    
+    def update_vectors(self):
+
+        self.forwards = np.array(
+            [
+                np.cos(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi)),
+                np.sin(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi)),
+                np.sin(np.deg2rad(self.phi))
+            ],
+            dtype = np.float32
+        )
+
+        globalUp = np.array([0,0,1], dtype=np.float32)
+
+        self.right = np.cross(self.forwards, globalUp)
+
+        self.up = np.cross(self.right, self.forwards)
+
+class Scene:
+
+
+    def __init__(self):
+
+        self.cubes = [
+            Cube(
+                position = [6,0,0],
+                eulers = [0,0,0]
+            ),
+        ]
+
+        self.player = Player(
+            position = [0,0,2]
+        )
+
+    def update(self, rate):
+
+        for cube in self.cubes:
+            cube.eulers[1] += 0.25 * rate
+            if cube.eulers[1] > 360:
+                cube.eulers[1] -= 360
+
+    def move_player(self, dPos):
+
+        dPos = np.array(dPos, dtype = np.float32)
+        self.player.position += dPos
+    
+    def spin_player(self, dTheta, dPhi):
+
+        self.player.theta += dTheta
+        if self.player.theta > 360:
+            self.player.theta -= 360
+        elif self.player.theta < 0:
+            self.player.theta += 360
         
+        self.player.phi = min(
+            89, max(-89, self.player.phi + dPhi)
+        )
+        self.player.update_vectors()
 
 class App:
 
 
+    def __init__(self, screenWidth, screenHeight):
 
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
 
-    def __init__(self):
-        """ Initialise the program """
-        #initialise pygame
-        pg.init()
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
-                                    pg.GL_CONTEXT_PROFILE_CORE)
-        #good size 960 720
-        pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
-        #initialise opengl
-        glClearColor(0.2, 0.2, 0.3, 1)
-        glEnable(GL_BLEND)
-        glEnable(GL_DEPTH_TEST)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        self.shader = self.createShader("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/shaders/vertex.txt", "C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/shaders/fragment.txt")
-        #self.shader = self.createShader("gfx/vertex.txt", "gfx/fragment.txt")
-        glUseProgram(self.shader)
-        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
-        #cube pos and stuff
-        self.cube = Cube(position = [ 0, 0, -8], eulers=[0, 0, 0])
-        #call cube mesh
-        self.cube_mesh = Mesh("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/models/n.obj")
+        self.renderer = GraphicsEngine()
 
-        
-        
-                    
-        #self.wood_texture = Material("D:/git/revolver-lite/pythonEngine/gfx/Screenshot (8).png")
-        self.wood_texture = Material("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/gfx/woodrte.jpg")
-            
-        projection_transform = pyrr.matrix44.create_perspective_projection( fovy=45, aspect=640/480, near=0.1, far=10, dtype=np.float32)
+        self.scene = Scene()
 
-        glUniformMatrix4fv( glGetUniformLocation(self.shader, "projection"), 1, GL_FALSE, projection_transform)
+        self.lastTime = pg.time.get_ticks()
+        self.currentTime = 0
+        self.numFrames = 0
+        self.frameTime = 0
+        self.lightCount = 0
 
-        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
-        
         self.mainLoop()
 
     def mainLoop(self):
-        """ Run the app """
-
         running = True
         while (running):
             #check events
             for event in pg.event.get():
                 if (event.type == pg.QUIT):
                     running = False
-
-            #cube update
-            self.cube.eulers[2] += 0.2
-            if (self.cube.eulers[2] > 360):
-                self.cube.eulers[2] -= 360
-                
-
-            #refresh screen
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glUseProgram(self.shader)
-            self.wood_texture.use()
-
-            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
-
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform, 
-                m2=pyrr.matrix44.create_from_eulers(
-                    
-                    eulers= np.radians(self.cube.eulers), dtype=np.float32
-                )
-            )
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform, 
-                m2=pyrr.matrix44.create_from_translation(
-                    vec=self.cube.position,
-                    dtype=np.float32
-                )
-            )
-            glUniformMatrix4fv(self.modelMatrixLocation, 1, GL_FALSE, model_transform)
-            glBindVertexArray(self.cube_mesh.vao)
-            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
-
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        running = False
             
+            self.handleKeys()
+            self.handleMouse()
 
-            pg.display.flip()
+            self.scene.update(self.frameTime * 0.05)
+            
+            self.renderer.render(self.scene)
 
             #timing
-            self.clock.tick(60)
+            self.calculateFramerate()
         self.quit()
 
+    def handleKeys(self):
+
+        keys = pg.key.get_pressed()
+        combo = 0
+        directionModifier = 0
+        """
+        w: 1 -> 0 degrees
+        a: 2 -> 90 degrees
+        w & a: 3 -> 45 degrees
+        s: 4 -> 180 degrees
+        w & s: 5 -> x
+        a & s: 6 -> 135 degrees
+        w & a & s: 7 -> 90 degrees
+        d: 8 -> 270 degrees
+        w & d: 9 -> 315 degrees
+        a & d: 10 -> x
+        w & a & d: 11 -> 0 degrees
+        s & d: 12 -> 225 degrees
+        w & s & d: 13 -> 270 degrees
+        a & s & d: 14 -> 180 degrees
+        w & a & s & d: 15 -> x
+        """
+
+        if keys[pg.K_w]:
+            combo += 1
+        if keys[pg.K_a]:
+            combo += 2
+        if keys[pg.K_s]:
+            combo += 4
+        if keys[pg.K_d]:
+            combo += 8
+        
+        if combo > 0:
+            if combo == 3:
+                directionModifier = 45
+            elif combo == 2 or combo == 7:
+                directionModifier = 90
+            elif combo == 6:
+                directionModifier = 135
+            elif combo == 4 or combo == 14:
+                directionModifier = 180
+            elif combo == 12:
+                directionModifier = 225
+            elif combo == 8 or combo == 13:
+                directionModifier = 270
+            elif combo == 9:
+                directionModifier = 315
+            
+            dPos = [
+                self.frameTime * 0.025 * np.cos(np.deg2rad(self.scene.player.theta + directionModifier)),
+                self.frameTime * 0.025 * np.sin(np.deg2rad(self.scene.player.theta + directionModifier)),
+                0
+            ]
+
+            self.scene.move_player(dPos)
+
+    def handleMouse(self):
+
+        (x,y) = pg.mouse.get_pos()
+        theta_increment = self.frameTime * 0.05 * ((self.screenWidth // 2) - x)
+        phi_increment = self.frameTime * 0.05 * ((self.screenHeight // 2) - y)
+        self.scene.spin_player(theta_increment, phi_increment)
+        pg.mouse.set_pos((self.screenWidth // 2,self.screenHeight // 2))
+
+    def calculateFramerate(self):
+
+        self.currentTime = pg.time.get_ticks()
+        delta = self.currentTime - self.lastTime
+        if (delta >= 1000):
+            framerate = max(1,int(1000.0 * self.numFrames/delta))
+            pg.display.set_caption(f"Running at {framerate} fps.")
+            self.lastTime = self.currentTime
+            self.numFrames = -1
+            self.frameTime = float(1000.0 / max(1,framerate))
+        self.numFrames += 1
+
+    def quit(self):
+        
+        self.renderer.destroy()
+
+class GraphicsEngine:
+
+
+    def __init__(self):
+
+        #initialise pygame
+        pg.init()
+        pg.mouse.set_visible(False)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+
+        
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                    pg.GL_CONTEXT_PROFILE_CORE) 
+        pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
+        #initialise opengl
+        glClearColor(0.0, 0.0, 0.0, 1)
+        self.shader = self.createShader("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/shaders/vertex.txt", "C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/shaders/fragment.txt")
+        glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/imageTexture"), 0)
+        glEnable(GL_DEPTH_TEST)
+
+        self.wood_texture = Material("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/gfx/woodrte.jpg")
+        self.cube_mesh = Mesh("C:/Users/user/Documents/GitHub/revolver-lite/pythonEngine/models/n.obj")
+
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy = 45, aspect = 640/480, 
+            near = 0.1, far = 50, dtype=np.float32
+        )
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader,"projection"),
+            1, GL_FALSE, projection_transform
+        )
+        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
+        self.viewMatrixLocation = glGetUniformLocation(self.shader, "view")
+    
     def createShader(self, vertexFilepath, fragmentFilepath):
 
         with open(vertexFilepath,'r') as f:
@@ -118,39 +248,72 @@ class App:
         
         shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
                                 compileShader(fragment_src, GL_FRAGMENT_SHADER))
+        
         return shader
 
-    def quit(self):
-        """ cleanup the app, run exit code """
+    def render(self, scene):
+
+        #refresh screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glUseProgram(self.shader)
+
+        view_transform = pyrr.matrix44.create_look_at(
+            eye = scene.player.position,
+            target = scene.player.position + scene.player.forwards,
+            up = scene.player.up, dtype = np.float32
+        )
+        glUniformMatrix4fv(self.viewMatrixLocation, 1, GL_FALSE, view_transform)
+
+        for cube in scene.cubes:
+
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(cube.eulers), dtype=np.float32
+                )
+            )
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=np.array(cube.position),dtype=np.float32
+                )
+            )
+            glUniformMatrix4fv(self.modelMatrixLocation,1,GL_FALSE,model_transform)
+            self.wood_texture.use()
+            glBindVertexArray(self.cube_mesh.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
+
+            pg.display.flip()
+
+    def destroy(self):
 
         self.cube_mesh.destroy()
         self.wood_texture.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
 
-#triangle
 class Mesh:
 
-    def __init__(self, filepath):
-        self.vertices = self.loadMesh(filepath)
-        
-        self.vertex_count = len(self.vertices) // 5
+
+    def __init__(self, filename):
+        # x, y, z, s, t, nx, ny, nz
+        self.vertices = self.loadMesh(filename)
+        self.vertex_count = len(self.vertices)//8
         self.vertices = np.array(self.vertices, dtype=np.float32)
-        
 
         self.vao = glGenVertexArrays(1)
         glBindVertexArray(self.vao)
         self.vbo = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        #position
         glEnableVertexAttribArray(0)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-        
+        #texture
         glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
-
-        
-
+    
     def loadMesh(self, filename):
 
         #raw, unassembled data
@@ -230,29 +393,27 @@ class Mesh:
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1,(self.vbo,))
 
-   
 class Material:
 
+    
     def __init__(self, filepath):
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        image = pg.image.load(filepath).convert_alpha()
+        image = pg.image.load(filepath).convert()
         image_width,image_height = image.get_rect().size
         img_data = pg.image.tostring(image,'RGBA')
         glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,image_width,image_height,0,GL_RGBA,GL_UNSIGNED_BYTE,img_data)
         glGenerateMipmap(GL_TEXTURE_2D)
+
     def use(self):
         glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glBindTexture(GL_TEXTURE_2D,self.texture)
 
     def destroy(self):
         glDeleteTextures(1, (self.texture,))
-        
 
-
-if __name__ == "__main__":
-    myApp = App()
+myApp = App(800,600)
