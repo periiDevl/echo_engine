53077b3c7cff4f40993633efe1ce4c5ac65f8c6c
periiProggramerGuy <jonathan.peri56@gmail.com>
2022-07-13 05:39:46 +0300

    nothing

--- pythonEngine/TEST.py
+++ pythonEngine/TEST.py
@@ -346,6 +346,7 @@ class GraphicsEngine:
         glClearColor(0.0, 0.0, 0.0, 1)
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_BLEND)
+        
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         self.create_framebuffers()
